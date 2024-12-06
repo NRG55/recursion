c204bac79649408b2971452723d7908bf58dc8d2
NRG55 <maloj_1980@yahoo.fr>
2024-12-06 15:35:52 +0000

    Added a function fibsRec which takes a number and returns an array containing that many numbers from the Fibonacci sequence (using recursion).

--- fibonacci.js
+++ fibonacci.js
@@ -9,7 +9,7 @@
 console.log("test")
 
 function fibs(number) {
-    const array = [0, 1];
+    const array = [0, 1];   
 
     for (let i = 2; i < number; i++) {
         array[i] = array[i-1] + array[i-2];
@@ -19,3 +19,16 @@ function fibs(number) {
 
 console.log(fibs(8));
 
+function fibsRec(number, array = [0, 1]) { 
+    console.log("This was printed recursively");  
+
+    if (array.length === number) {
+        return array;
+    }    
+
+    array.push(array[array.length - 1] + array[array.length - 2]);
+
+    return fibsRec(number, array);
+}
+
+console.log(fibsRec(8));
