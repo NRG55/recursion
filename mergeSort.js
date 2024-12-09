2870279949402b73054f58fda50b3aa38eafeec3
NRG55 <maloj_1980@yahoo.fr>
2024-12-09 17:38:45 +0000

    mergeSort.js comments added.

--- mergeSort.js
+++ mergeSort.js
@@ -1,29 +1,47 @@
+//--------------MERGE verson 1---------------
+// Functions merge and mergeVersionTwo merge two sorted arrays 
+
 function merge(firstArray, secondArray) {
     const mergedArray = [];
-    let firstIndex = 0;
-    let secondIndex = 0;
+    let firstArrayIndex = 0;
+    let secondArrayIndex = 0;
 
-    while (firstIndex < firstArray.length && secondIndex < secondArray.length) {
-        if (firstArray[firstIndex] < secondArray[secondIndex]) {
-            mergedArray.push(firstArray[firstIndex]);
-            firstIndex++;
+    while (firstArrayIndex < firstArray.length && secondArrayIndex < secondArray.length) {
+        if (firstArray[firstArrayIndex] < secondArray[secondArrayIndex]) {
+            mergedArray.push(firstArray[firstArrayIndex]);
+            firstArrayIndex++;
         } else {
-            mergedArray.push(secondArray[secondIndex]);
-            secondIndex++;
+            mergedArray.push(secondArray[secondArrayIndex]);
+            secondArrayIndex++;
         };
     };
 
-    while (firstIndex < firstArray.length) {
-        mergedArray.push(firstArray[firstIndex]);
-        firstIndex++;
+    while (firstArrayIndex < firstArray.length) {
+        mergedArray.push(firstArray[firstArrayIndex]);
+        firstArrayIndex++;
     };
 
-    while (secondIndex < secondArray.length) {
-        mergedArray.push(secondArray[secondIndex]);
-        secondIndex++;
+    while (secondArrayIndex < secondArray.length) {
+        mergedArray.push(secondArray[secondArrayIndex]);
+        secondArrayIndex++;
     };
 
     return mergedArray;
+}
+
+//-------------MERGE Version 2----------------
+
+function mergeVersionTwo(leftArray, rightArray) {
+    let mergedArray = [];
+  
+    while (leftArray.length && rightArray.length) {
+      if (leftArray[0] < rightArray[0]) {
+        mergedArray.push(leftArray.shift());
+      } else {
+        mergedArray.push(rightArray.shift());
+      }
+    }  
+    return [...mergedArray, ...leftArray, ...rightArray];
 }
 
 function mergeSort(array) {
@@ -33,29 +51,17 @@ function mergeSort(array) {
 
     const middleOfArray = Math.floor(array.length / 2);
   
-    const leftSideOfArray = mergeSort(array.slice(0, middleOfArray));    
-    const rightSideOfArray = mergeSort(array.slice(middleOfArray));    
-   
-    return merge(leftSideOfArray, rightSideOfArray);
+    const leftSideOfArray = array.slice(0, middleOfArray);    
+    const rightSideOfArray = array.slice(middleOfArray);     
+    
+    return merge(mergeSort(leftSideOfArray), mergeSort(rightSideOfArray));
 }
 
 const array1 = [3, 2, 1, 13, 8, 5, 0, 1];
 const array2 = [105, 79, 100, 110];
 
+console.log(mergeVersionTwo([1, 2, 10, 13], [8, 9, 20, 23, 24, 63]));
+
 console.log(mergeSort(array1));
 console.log(mergeSort(array2));
 
-// function mergeOption2(leftArray, rightArray) {
-//     let mergedArray = [];
-  
-//     while (leftArray.length && rightArray.length) {
-//       if (leftArray[0] < rightArray[0]) {
-//         mergedArray.push(leftArray.shift());
-//       } else {
-//         mergedArray.push(rightArray.shift());
-//       }
-//     }  
-//     return [...mergedArray, ...leftArray, ...rightArray];
-//   }
-
-//   console.log(mergeOption2([1, 2, 10, 13], [8, 9, 20, 23, 24, 63]));
